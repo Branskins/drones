51f7ac02545300bbc00b514e7a77dfa094f3188e
Branskins <andresje17@gmail.com>
2025-11-28 00:40:42 -0600

    Update ledger

--- app/app/ledger.tsx
+++ app/app/ledger.tsx
@@ -2,8 +2,8 @@ import { createClient } from '@/utils/supabase';
 
 export default async function Ledgers() {
   const supabase = await createClient();
-  const { data: ledgers } = await supabase
-    .from("ledgers")
+  const { data: sales } = await supabase
+    .from("sales")
     .select()
     .limit(10);
 
@@ -12,16 +12,20 @@ export default async function Ledgers() {
       <thead>
         <tr>
           <th style={{ border: '1px solid #ddd', padding: '8px', textAlign: 'left' }}>Ref ID</th>
-          <th style={{ border: '1px solid #ddd', padding: '8px', textAlign: 'right' }}>Balance</th>
+          <th style={{ border: '1px solid #ddd', padding: '8px', textAlign: 'left' }}>Asset</th>
           <th style={{ border: '1px solid #ddd', padding: '8px', textAlign: 'right' }}>Amount</th>
+          <th style={{ border: '1px solid #ddd', padding: '8px', textAlign: 'right' }}>Buy</th>
+          <th style={{ border: '1px solid #ddd', padding: '8px', textAlign: 'right' }}>Purchase Price</th>
         </tr>
       </thead>
       <tbody>
-        {ledgers?.map((ledger) => (
-          <tr key={ledger.ledger_id}>
-            <td style={{ border: '1px solid #ddd', padding: '8px' }}>{ledger.refid}</td>
-            <td style={{ border: '1px solid #ddd', padding: '8px', textAlign: 'right' }}>{ledger.balance}</td>
-            <td style={{ border: '1px solid #ddd', padding: '8px', textAlign: 'right' }}>{ledger.amount}</td>
+        {sales?.map((sale) => (
+          <tr key={sale.refid}>
+            <td style={{ border: '1px solid #ddd', padding: '8px' }}>{sale.refid}</td>
+            <td style={{ border: '1px solid #ddd', padding: '8px' }}>{sale.asset}</td>
+            <td style={{ border: '1px solid #ddd', padding: '8px', textAlign: 'right' }}>{sale.amount_non_zusd}</td>
+            <td style={{ border: '1px solid #ddd', padding: '8px', textAlign: 'right' }}>{sale.amount_zusd}</td>
+            <td style={{ border: '1px solid #ddd', padding: '8px', textAlign: 'right' }}>{sale.purchase_price}</td>
           </tr>
         ))}
       </tbody>
