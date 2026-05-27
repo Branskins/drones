526f5c110eead0cec078e8354eac419afa36d30c
Branskins <andresje17@gmail.com>
2026-05-26 23:27:21 -0600

    Add SQLFluff configuration and format SQL scripts for consistency

--- trades.ddl.sql
+++ trades.ddl.sql
@@ -1,13 +1,13 @@
-create table public.trades (
-  base_ledger_id text not null,
-  quote_ledger_id text not null,
-  asset text null,
-  status text null,
-  side text null,
-  volume double precision null,
-  price_usd double precision null,
-  cost_usd double precision null,
-  fee_usd double precision null,
-  executed_at timestamp without time zone null,
-  constraint trades_pkey primary key (base_ledger_id, quote_ledger_id)
+CREATE TABLE public.trades (
+    base_ledger_id text NOT NULL,
+    quote_ledger_id text NOT NULL,
+    asset text NULL,
+    status text NULL,
+    side text NULL,
+    volume double precision NULL,
+    price_usd double precision NULL,
+    cost_usd double precision NULL,
+    fee_usd double precision NULL,
+    executed_at timestamp without time zone NULL,
+    CONSTRAINT trades_pkey PRIMARY KEY (base_ledger_id, quote_ledger_id)
 ) TABLESPACE pg_default;
